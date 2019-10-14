17579656bd470f26b9caaee79c94a6493cd1015d
Márcio Barbosa <marciobarbosamobile@gmail.com>
2019-10-13 23:35:38 -0300

    Exercicio 10 queries do Spring Data

--- src/main/java/com/projetoapi/contato/repositorio/ContatoRepositorio.java
+++ src/main/java/com/projetoapi/contato/repositorio/ContatoRepositorio.java
@@ -1,12 +1,34 @@
 package com.projetoapi.contato.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.projetoapi.contato.entidades.Contato;
+import com.projetoapi.contato.entidades.Telefone;
 
 
 @Repository
 public interface ContatoRepositorio extends JpaRepository<Contato, Long>{
 
+	List<Contato> findByNome(String nome);
+
+	List<Contato> findByNomeNot(String nome);
+
+	List<Contato> findByNomeContaining(String nome);
+
+	List<Contato> findByNomeAndEmail(String nome, String email);
+
+	List<Contato> findByNomeAndEmailNot(String nome, String email);
+	
+	List<Contato> findByTelefonesIsNull();
+	
+	List<Contato> findByTelefonesIn(List<Telefone> telefones);
+	
+	List<Contato> findByTelefonesNotIn(List<Telefone> telefones);
+	
+	List<Contato> findByTelefonesInOrderByNomeDesc(List<Telefone> telefones);
+	
+	List<Contato> findByNomeContainingAndTelefonesIn(String nome, List<Telefone> telefones);
 }
